209477713686fbe0a1f53562a1a9eaf48dbecdb5
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-05-30 14:58:23 +0300

    JDK-8264303 Create implementation for NSAccessibilityTabGroup protocol peer

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -112,7 +112,7 @@ static jobject sAccessibilityClass = NULL;
     /*
      * Here we should keep all the mapping between the accessibility roles and implementing classes
      */
-    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:41];
+    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:42];
 
     [rolesMap setObject:@"ButtonAccessibility" forKey:@"pushbutton"];
     [rolesMap setObject:@"ImageAccessibility" forKey:@"icon"];
@@ -137,6 +137,7 @@ static jobject sAccessibilityClass = NULL;
     [rolesMap setObject:@"NavigableTextAccessibility" forKey:@"passwordtext"];
     [rolesMap setObject:@"NavigableTextAccessibility" forKey:@"dateeditor"];
     [rolesMap setObject:@"ComboBoxAccessibility" forKey:@"combobox"];
+    [rolesMap setObject:@"TabGroupAccessibility" forKey:@"pagetablist"];
 
     /*
      * All the components below should be ignored by the accessibility subsystem,
